84e81dd263ee3b4b001818c05567995d1f84650e
twotanawin <tanawin.twox@gmail.com>
2024-08-25 23:41:36 +0700

    loop

--- main.go
+++ main.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"fmt"
-
-	twoISE "github.com/TwoTanawin/go-example.git/twoise"
-	"github.com/google/uuid"
 )
 
 func main() {
-	id := uuid.New()
-	fmt.Printf("Hello Go 2")
-	fmt.Printf("UUID: %s", id)
-	twoISE.SeyHelloTwoISE()
-	twoISE.SeyTest()
+	fullName := "TwoISE"
+	age := 24
+	fmt.Printf("Hello %s age: %d", fullName, age)
+
+	fullName = "Nice Two"
+
+	age *= age
+
+	fmt.Println(fullName, age)
+
+	//for loop
+	fmt.Printf("for loop\n")
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("number : %d\n", i)
+	}
+
+	// do while
+	fmt.Printf("do while loop\n")
+	j := 0
+	for {
+		fmt.Printf("number : %d\n", j)
+		j++
+		if j > 10 {
+			break
+		}
+	}
+
+	//while loop
+	fmt.Printf("while loop\n")
+	k := 1
+	for k <= 10 {
+		fmt.Printf("number : %d\n", k)
+		k++
+	}
 }
